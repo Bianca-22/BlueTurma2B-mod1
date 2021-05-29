657810c30cf6b8d9cd0e8246db09bd82d5dfcc8c
Bianca-22 <83734803+Bianca-22@users.noreply.github.com>
2021-05-29 12:05:13 -0300

    Add files via upload

--- Aula 09/Exercício 06.py
+++ Aula 09/Exercício 06.py
@@ -1,1 +1,8 @@
 #Ex 06:
+materia = int(input('Quantas matérias você tem? '))
+lista = []
+for i in range(1,materia+1):
+    nota = float(input('Qual a nota na matéria: '))
+    lista.append(nota)
+    print(lista)
+    print('A média geral do aluno é de ', sum(lista)/4)
